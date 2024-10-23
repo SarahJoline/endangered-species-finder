74c92688c1dbe7be0512e08d85a836a706a01f2e
Sarah Naas <sjnaas@gmail.com>
2024-10-23 14:57:47 +0200

    Map

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
+import React from "react";
+import Map from "react-map-gl";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">Hello</header>
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <Map
+        mapboxAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+        latitude={37.7577}
+        longitude={-122.4376}
+        zoom={8}
+        mapStyle="mapbox://styles/mapbox/streets-v11"
+        width="100%"
+        height="100%"
+      />
     </div>
   );
 }

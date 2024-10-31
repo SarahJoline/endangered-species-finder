1f32131ec7907be7833fb65aa76a61952e17d9be
Sarah Naas <sjnaas@gmail.com>
2024-10-31 17:12:58 +0100

    removed cors

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const allSpecies = require("common-species");
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
 // Middleware
-app.use(cors());
 app.use(express.json());
+app.get("/species/:name", (req, res) => {
+  //   const name = req.params.name.toLowerCase();
+  //   const species = allSpecies.filter((item) =>
+  //     item.commonName.toLowerCase().includes(name)
+  //   );
+  res.json(allSpecies);
+});
 
 // Start the server
 app.listen(PORT, () => {

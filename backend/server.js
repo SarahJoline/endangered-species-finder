9a7166d58a5147040c83933efb1626b42114a51d
Sarah Naas <sjnaas@gmail.com>
2024-10-31 21:57:45 +0100

    fetching categories

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,38 @@
 const express = require("express");
-const cors = require("cors");
 require("dotenv").config();
-const allSpecies = require("common-species");
+const commonSpecies = require("common-species");
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
 // Middleware
 app.use(express.json());
+let speciesData = [];
+commonSpecies.loadSpeciesData((dataSet) => {
+  speciesData = dataSet;
+  console.log(`Species data loaded with ${dataSet.length} entries`);
+});
+
+app.get("/api/species/categories", (req, res) => {
+  if (speciesData.length === 0) {
+    return res.status(503).json({ error: "Species data not loaded yet." });
+  }
+
+  // Extract unique category names
+  const categoryNames = speciesData.map((species) => species.category_name);
+  const uniqueCategoryNames = [...new Set(categoryNames)];
+
+  res.json(uniqueCategoryNames);
+});
 app.get("/species/:name", (req, res) => {
-  //   const name = req.params.name.toLowerCase();
-  //   const species = allSpecies.filter((item) =>
-  //     item.commonName.toLowerCase().includes(name)
-  //   );
-  res.json(allSpecies);
+  if (speciesData.length === 0) {
+    return res.status(503).json({ error: "Species data not loaded yet." });
+  }
+  const commonName = "bear"; // Get name from query
+  const filteredSpecies = speciesData.filter((species) =>
+    species.common_name.toLowerCase().includes(commonName.toLowerCase())
+  );
+  res.json(filteredSpecies);
 });
 
 // Start the server

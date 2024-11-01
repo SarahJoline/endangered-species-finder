19f71f6958c0c6f8061774ca314895d6cc8b2314
Sarah Naas <sjnaas@gmail.com>
2024-11-01 14:56:31 +0100

    display common name

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -1,8 +1,11 @@
 import React from "react";
 
 function card({ sp }) {
-  console.log(sp);
-  return <div>card</div>;
+  return (
+    <div key={sp.taxonomy.id}>
+      <p>{sp.common_name}</p>
+    </div>
+  );
 }
 
 export default card;

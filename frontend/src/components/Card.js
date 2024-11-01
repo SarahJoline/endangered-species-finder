f5f962daaa6fa35da923f5fd91f42497760a8c7a
Sarah Naas <sjnaas@gmail.com>
2024-11-01 15:31:10 +0100

    added some styles

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -1,14 +1,19 @@
 import React from "react";
+import styled from "styled-components";
+
+const StyledButton = styled.button`
+  background-color: transparent;
+  border: none;
+`;
 
 function card({ sp }) {
   console.log(sp);
+
   return (
     <div key={sp.taxonomy.id}>
-      <button>
-        <p>
-          {sp.common_name} ({sp.taxonomy.species})
-        </p>
-      </button>
+      <StyledButton>
+        {sp.common_name} ({sp.taxonomy.species})
+      </StyledButton>
     </div>
   );
 }

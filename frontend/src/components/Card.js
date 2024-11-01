3a19d2bfc2a16f869f587f89a4c1724c44d7fee9
Sarah Naas <sjnaas@gmail.com>
2024-11-01 15:00:13 +0100

    show taxonomy name

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -1,9 +1,12 @@
 import React from "react";
 
 function card({ sp }) {
+  console.log(sp);
   return (
     <div key={sp.taxonomy.id}>
-      <p>{sp.common_name}</p>
+      <p>
+        {sp.common_name} ({sp.taxonomy.species})
+      </p>
     </div>
   );
 }

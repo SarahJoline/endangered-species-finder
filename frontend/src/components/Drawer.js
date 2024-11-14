941f7510e28e252903028aeadac6a4bab4806a43
Sarah Naas <sjnaas@gmail.com>
2024-11-14 10:57:52 +0100

    aside

--- frontend/src/components/Drawer.js
+++ frontend/src/components/Drawer.js
@@ -1,6 +1,19 @@
 import styled from "styled-components";
 
-const StyledDrawer = styled.div``;
+const StyledDrawer = styled.aside`
+  position: fixed;
+  height: 100%;
+  top: 0;
+  background-color: #ffffff;
+  overflow-y: auto;
+  width: 100%;
+  z-index: 2;
+  padding: 20px;
+  @media only screen and (min-width: 48em) {
+    max-width: 678px;
+    padding: 2.5em;
+  }
+`;
 
 function Drawer({ selectedSpecies }) {
   return (

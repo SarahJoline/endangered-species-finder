05448cbea0cd1955f7cc6c891b19e5726ac15714
Sarah Naas <sjnaas@gmail.com>
2024-12-30 14:06:27 +0100

    nav style + button

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -1,14 +1,29 @@
 import styled from "styled-components";
 
 const StyledNavigation = styled.div`
-  height: 50px;
+  min-height: 50px;
   flex: 1;
   background: linear-gradient(180deg, #4b4d61, #34364a);
+  color: #fff;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+`;
+
+const StyledSearchByButton = styled.button`
+  border: none;
+  background-color: transparent;
   color: #fff;
 `;
 
 function Navigation() {
-  return <StyledNavigation>What do I want to put here?</StyledNavigation>;
+  return (
+    <StyledNavigation>
+      <StyledSearchByButton onClick={() => console.log("dropdown")}>
+        Search by...
+      </StyledSearchByButton>
+    </StyledNavigation>
+  );
 }
 
 export default Navigation;
